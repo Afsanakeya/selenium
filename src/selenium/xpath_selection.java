b7bff520f747e5ead71780a6a15c3633138c52a3
afsanakeya <afsanakeya@localhost>
2020-06-16 08:32:08 -0400

    slenium commit

--- src/selenium/xpath_selection.java
+++ src/selenium/xpath_selection.java
@@ -1,5 +1,6 @@
 package selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,8 +10,13 @@ public static void main(String[] args) {
 System.setProperty("webdriver.chrome.driver", "/Users/afsanakeya/Documents/Automation/selenium/Driver/chromedriver");
 WebDriver driver=new ChromeDriver();
 driver.get("https://www.facebook.com/");	
+//linktext
+//driver.findElement(By.linkText("Forgot account?")).click();
+//partial linktext
+driver.findElement(By.partialLinkText("Forgot")).click();
 
-
+//CSS selector
+driver.findElement(By.cssSelector("input[type=\"email\"]")).getText();
 
 
 }
