fd7388e5ae850be662a69f21d6029c675b8ec302
afsanakeya <afsanakeya@localhost>
2020-06-15 23:11:30 -0400

    commit1

---
+++ src/selenium/xpath_selection.java
@@ -0,0 +1,18 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class xpath_selection {
+
+public static void main(String[] args) {
+System.setProperty("webdriver.chrome.driver", "/Users/afsanakeya/Documents/Automation/selenium/Driver/chromedriver");
+WebDriver driver=new ChromeDriver();
+driver.get("https://www.facebook.com/");	
+
+
+
+
+}
+
+}
